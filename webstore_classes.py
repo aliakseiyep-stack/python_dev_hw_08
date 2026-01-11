468824b30980da34a39bb0dc725af3fb292f01ab
aliakseiyep-stack <familie.yep@gmail.com>
2026-01-11 06:47:51 +0100

    HW-08 added classes Customer Order Discount

--- webstore_classes.py
+++ webstore_classes.py
@@ -1,5 +1,30 @@
-class Order:
+class Product:
+  
+  def __init__(self, product_name, product_price):
+    self.product_name = product_name
+    self.product_price = product_price
 
-  def __init__(self, product_id, price):
-    self.productID = product_id
-    self.productPrice = price
+
+class Customer:
+  orders = []
+  def __init__(self, customer_name):
+    self.customer_name = customer_name
+    
+  def add_order(self, order: Order):
+    self.orders.append(order)
+
+
+class Order:
+  
+  products = []
+
+  def add_pruduct(self, product: Product):
+    self.products.append(product)
+
+
+class Discount:
+
+  def __init__(self, discount_description, discount_percent):
+    self.description = discount_description
+    self.discount_percent = discount_percent
+
